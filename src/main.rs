f4ff68361c29099792ce7addacd430832d2060ad
Richard Hao <richard@0xdev.dev>
2023-06-20 17:08:06 +0800

    feat: print out the response from ipinfo.io

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::blocking::{get, Response};
+use reqwest::StatusCode;
+use serde::Deserialize;
+use tabled::{Table, Tabled};
+
+#[derive(Deserialize, Tabled)]
+struct IpInfo {
+    ip: String,
+    city: String,
+    region: String,
+    country: String,
+    loc: String,
+    org: String,
+    postal: String,
+    timezone: String,
+}
+
+fn main() -> Result<(), reqwest::Error> {
+    let url: &str = "https://ipinfo.io";
+    let resp: Response = get(url)?;
+    match resp.status() {
+        StatusCode::OK => match resp.json::<IpInfo>() {
+            Ok(info) => pretty_print(info),
+            Err(_) => panic!("The response JSON is not valid"),
+        },
+        other => {
+            panic!("Something unexpected happened: {:?}", other);
+        }
+    }
+
+    Ok(())
+}
+
+fn pretty_print(info: IpInfo) {
+    let ipinfos = vec![info];
+    let table = Table::new(ipinfos).to_string();
+    println!("{}", table);
 }
