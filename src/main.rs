9635b6c1d91454facebda6978a2c82153aa79d47
Richard Hao <richard@0xdev.dev>
2023-06-28 01:31:34 +0800

    feat: refactor IP info request function and add unit test
    - Add `mockito` as a dev dependency
    - Move `get` request to a separate function `request_ipinfo`
    - Add a unit test for `request_ipinfo` function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use reqwest::blocking::{get, Response};
+use reqwest::blocking::get;
 use reqwest::StatusCode;
 use serde::Deserialize;
 use tabled::{Table, Tabled};
@@ -16,8 +16,12 @@ struct IpInfo {
 }
 
 fn main() -> Result<(), reqwest::Error> {
-    let url: &str = "https://ipinfo.io";
-    let resp: Response = get(url)?;
+    let url = "https://ipinfo.io";
+    request_ipinfo(url)
+}
+
+fn request_ipinfo(url: &str) -> Result<(), reqwest::Error> {
+    let resp = get(url)?;
     match resp.status() {
         StatusCode::OK => match resp.json::<IpInfo>() {
             Ok(info) => pretty_print(info),
@@ -35,4 +39,29 @@ fn pretty_print(info: IpInfo) {
     let ipinfos = vec![info];
     let table = Table::new(ipinfos).to_string();
     println!("{}", table);
+}
+
+#[cfg(test)]
+#[test]
+fn test_request_ipinfo() {
+    let mut s = mockito::Server::new();
+    let url = s.url();
+    let body = r#"
+        {
+            "ip": "1.2.33.115",
+            "hostname": "ec2-13-212-33-105.ap-southeast-1.compute.amazonaws.com",
+            "city": "Singapore",
+            "region": "Singapore",
+            "country": "SG",
+            "loc": "1.2795,103.8682",
+            "org": "AS16509 Amazon.com, Inc.",
+            "postal": "019396",
+            "timezone": "Asia/Singapore",
+            "readme": "https://ipinfo.io/missingauth"
+        }
+    "#;
+    s.mock("GET", "/").with_status(200).with_body(body).create();
+
+    let result = request_ipinfo(url.as_str());
+    assert!(result.is_ok());
 }
