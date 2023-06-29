507299fa0ec56d0bdd80f4167ff70288029ee3d1
Richard Hao <richard@0xdev.dev>
2023-06-29 09:37:40 +0800

    chore: move tests to the test mods

--- src/main.rs
+++ src/main.rs
@@ -42,11 +42,14 @@ fn pretty_print(info: IpInfo) {
 }
 
 #[cfg(test)]
-#[test]
-fn test_request_ipinfo() {
-    let mut s = mockito::Server::new();
-    let url = s.url();
-    let body = r#"
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_request_ipinfo() {
+        let mut s = mockito::Server::new();
+        let url = s.url();
+        let body = r#"
         {
             "ip": "1.2.33.115",
             "hostname": "ec2-13-212-33-105.ap-southeast-1.compute.amazonaws.com",
@@ -60,8 +63,9 @@ fn test_request_ipinfo() {
             "readme": "https://ipinfo.io/missingauth"
         }
     "#;
-    s.mock("GET", "/").with_status(200).with_body(body).create();
+        s.mock("GET", "/").with_status(200).with_body(body).create();
 
-    let result = request_ipinfo(url.as_str());
-    assert!(result.is_ok());
+        let result = request_ipinfo(url.as_str());
+        assert!(result.is_ok());
+    }
 }
